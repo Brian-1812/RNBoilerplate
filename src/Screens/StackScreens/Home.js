70bcac8e75755548f7a2853ea2521bbad693aeed
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2021-05-16 18:51:04 +0900

    Vector icons added

--- src/Screens/StackScreens/Home.js
+++ src/Screens/StackScreens/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 import I18n from '../../Utils/i18n'
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text>{I18n.t('hello')}</Text>
+      <Icon name="home" size={30} color="blue" />
     </View>
   )
 }
