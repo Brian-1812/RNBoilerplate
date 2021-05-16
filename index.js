8e0facbaeb8321aa9c728cd9459a4c3dd9129f1b
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2021-05-16 17:15:19 +0900

    Navigation, redux integrated

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react'
+import { AppRegistry } from 'react-native';
+import { Provider as StoreProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import configureStore from './src/store/Store';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  const { store, persistor } = configureStore();
+  return (
+    <StoreProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </StoreProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
